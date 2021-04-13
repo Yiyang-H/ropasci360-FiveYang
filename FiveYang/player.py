8705acecc75ecac123e325823d94b1646cf585e4
Siyang Qiu <siyang.qiu@student.unimelb.edu.au>
2021-04-13 22:19:14 +0800

    Fixed bugs for update method

--- FiveYang/player.py
+++ FiveYang/player.py
@@ -13,12 +13,9 @@ class Player:
         play as Upper), or the string "lower" (if the instance will play
         as Lower).
         """
-        self.type = player
+        self.player = player
         self.num_throws_left = 9
-        self.num_defeated_tokens = 0
-
         self.num_opponent_throws_left = 9
-        self.num_opponent_defeated_tokens = 0
 
         self.tokens = {"r":[], "p":[], "s":[]}
         self.opponent_tokens = {"r":[], "p":[], "s":[]}
@@ -31,6 +28,8 @@ class Player:
         of the game, select an action to play this turn.
         """
         # put your code here
+        if self.player == "upper": return Player.throw_action("r", (4, -2))
+        else: return Player.throw_action("r", (-4, 2))
     
     def update(self, opponent_action, player_action):
         """
@@ -44,18 +43,20 @@ class Player:
         player = None
         if player_action[0] == "THROW":
             player = Token(player_action[1], player_action[2], False)
-            self.tokens[player_action[1]].append[player]
+            self.tokens[player_action[1]].append(player)
+            self.num_throws_left -= 1
         else:
-            player = self.tokens[player_action[1]]
+            player = self.tokens_dict[player_action[1]]
             player.location = player_action[2]
             
 
         opponent = None
         if opponent_action[0] == "THROW":
             opponent = Token(opponent_action[1], opponent_action[2], True)
-            self.opponent_tokens[opponent_action[1]].append[opponent]
+            self.opponent_tokens[opponent_action[1]].append(opponent)
+            self.num_opponent_throws_left -= 1
         else:
-            opponent = self.opponent_tokens[opponent_action[1]]
+            opponent = self.opponent_tokens_dict[opponent_action[1]]
             opponent.location = opponent_action[2]
 
         # collision handling
@@ -70,7 +71,7 @@ class Player:
 
         for token in self.all_tokens_list:
             if token.location == location:
-                tokens_on_tile[token.type].append(token)
+                tokens_on_tile[token.token_type].append(token)
         
         if tokens_on_tile["r"]: remove_type.append("s")
         if tokens_on_tile["p"]: remove_type.append("r")
@@ -82,8 +83,8 @@ class Player:
 
     # remove a token
     def removeToken(self, token):
-        if not token.is_opponent: self.tokens[token.type].remove(token)
-        else: self.opponent_tokens[token.type].remove(token)
+        if not token.is_opponent: self.tokens[token.token_type].remove(token)
+        else: self.opponent_tokens[token.token_type].remove(token)
 
     # return the list of currently alive tokens on our side
     @property
@@ -99,7 +100,34 @@ class Player:
     def all_tokens_list(self):
         return self.tokens_list + self.opponent_tokens_list
 
+     # returns a dictionary {location: token}
+    
+    @property
+    def tokens_dict(self):
+        dic = {}
+        for token in self.tokens_list:
+            if token.location not in dic:
+                dic[token.location] = token
+        return dic
+
+    # returns a dictionary {location: token}
+    @property
+    def opponent_tokens_dict(self):
+        dic = {}
+        for token in self.opponent_tokens_list:
+            if token.location not in dic:
+                dic[token.location] = token
+        return dic
+
     #return the max row that is legal to throw to
     @property
     def max_throw_row(self):
         return 10 - self.throws_left
+
+    @staticmethod
+    def throw_action(token_type, destination):
+        return ("THROW", token_type, destination)
+
+    @staticmethod
+    def move_action(action_type, origin, destination):
+        return (action_type, origin, destination)
