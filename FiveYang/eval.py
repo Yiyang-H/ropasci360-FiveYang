2f13596def39c0ae0a600a2057b7537736b21e84
Siyang Qiu <siyang.qiu@student.unimelb.edu.au>
2021-04-25 14:02:56 +0800

    Rubric for evaluation function

---
+++ FiveYang/eval.py
@@ -0,0 +1,21 @@
+'''
+features
++
+    1. #throws
+    2. #opponent_tokens in player throw zone
+    3. #opponent_tokens eatable by player slide / swing actions
+    4. 3 piece gatherings?
+
+~
+    1. #tokens on board
+    2. #opponent tokens on board
+    3. Player: sum of (row_weight * current row of each token)
+    4. Opponent: sum of (row_weight * current row of each token)
+
+-
+    
+    1. #opponent_throws
+    2. #tokens in opponent throw zone
+    3. #tokens eatable by opponent slide / swing actions
+
+'''
