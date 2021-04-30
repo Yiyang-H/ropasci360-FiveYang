93490f6fde0afcc80d898cb789fb4892ed3deae5
Yiyang <yiyahuang@student.edu.au>
2021-04-30 13:55:34 +1000

    Implemented evaluaction function

--- FiveYang/board.py
+++ FiveYang/board.py
@@ -1,6 +1,8 @@
 from FiveYang.token import Token
 
 class Board:
+    weights = (1,1,1,1,1,0,0,0,0,-1,-1,-1,-1)
+
     def __init__(self):
         self.upper_num_throws_left = 9
         self.lower_num_throws_left = 9
@@ -83,8 +85,102 @@ class Board:
             self.lower_tokens[tokens.token_type].remove(token)
 
     def eval(self, is_upper):
-        pass
-    
+        total = 0
+        
+        # Feature A1: #ally throws
+        total += self.f1(is_upper) * self.weights[0]
+
+        # Feature A2: #opponent tokens in ally throw zone
+        total += self.f2(is_upper) * self.weights[1]
+
+        # Feature A3:
+        total += self.f3(is_upper) * self.weights[2]
+
+        # Feature A4:
+        total += self.f4(is_upper) * self.weights[3]
+
+        # Feature A5:
+        total += self.f5(is_upper) * self.weights[4]
+
+        # Feature E1:
+        total += self.f1(not is_upper) * self.weights[9]
+
+        # Feature E2:
+        total += self.f2(not is_upper) * self.weights[10]
+
+        # Feature E3:
+        total += self.f3(not is_upper) * self.weights[11]
+
+        # Feature E4:
+        total += self.f4(not is_upper) * self.weights[12]
+
+        # Feature E5:
+        total += self.f5(not is_upper) * self.weights[13]
+
+        return total
+
+    # Number of throws left
+    def f1(self, is_upper):
+        if is_upper:
+            return self.upper_num_throws_left
+        else:
+            return self.lower_num_throws_left
+
+    # Number of oppenent tokens in ally throw zone
+    def f2(self, is_upper):
+        number = 0
+        row = 0
+        if is_upper:
+            row = 4-(9-self.upper_num_throws_left)
+        else:
+            row = 4-(9-self.lower_num_throws_left)
+
+        if is_upper:
+            for token in self.lower_tokens_list:
+                if token.location[0] >= row:
+                    number += 1
+        else:
+            for token in self.upper_tokens_list:
+                if token.location[0] <= row * -1:
+                    number += 1
+        return number
+
+    def f3(self, is_upper):
+        eaten_list = []
+        ally_tokens_list = None
+        enemy_tokens_list = None
+        if is_upper:
+            ally_tokens_list = self.upper_tokens_list
+            enemy_tokens = self.lower_tokens
+        else:
+            ally_tokens_list = self.lower_tokens_list
+            enemy_tokens = self.upper_tokens
+        
+        for token in ally_tokens_list:
+            valid_moves = self.valid_moves(token)
+            for enemy_token in enemy_tokens[token.beat_type]:
+                if enemy_token.location in valid_moves and enemy_token not in eaten_list:
+                    eaten_list.append(enemy_token)
+
+        return len(eaten_list)
+
+    def f4(self, is_upper):
+        if is_upper:
+            return (9 - self.lower_num_throws_left) - len(self.lower_tokens_list)
+        else:
+            return (9 - self.upper_num_throws_left) - len(self.upper_tokens_list)
+
+    def f5(self, is_upper, ):
+        total = 0
+        if is_upper:
+            for token in self.upper_tokens_list:
+                total += token.location[0]
+        else:
+            for token in self.lower_tokens_list:
+                total -= token.location[0]
+
+        return total
+
     '''
     When do we need to put throw actions in successor?
     1. When we have no token on board
@@ -99,6 +195,38 @@ class Board:
     '''
     def successor(self, is_upper):
         pass
+
+    # simply find all possible moves by a token
+    def valid_moves(self, token):
+        neighbours = Board.find_neighbours(token.location)
+        far_neighbours = []
+        current_tokens_list = None
+        if token.is_upper:
+            current_tokens_list = self.upper_tokens_list
+        else:
+            current_tokens_list = self.lower_tokens_list
+        
+        for token in current_tokens_list:
+            if token.location in neighbours:
+                far_neighbours += Board.find_neighbours(token.location)
+        for location in farNeighbours:
+            if location not in neighbours:
+                neighbours.append(location)
+
+        return neighbours
+    
+    # finds all the neighbours on board of the given location
+    @staticmethod
+    def find_neighbours(location):
+        r = location[0]
+        q = location[1]
+        neighbours = [(r,q-1),(r,q+1),(r-1,q),(r+1,q),(r-1,q+1),(r+1,q-1)]
+        valid_neighbours = []
+        for r, q in neighbours:
+            if abs(r) <= 4 and abs(q) <= 4 and abs(r+q) <= 4: 
+                valid_neighbours.append((r,q))
+        
+        return valid_neighbours
 
     @property
     def upper_tokens_list(self):
